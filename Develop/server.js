4b12c6b08b7c50a4441a6757125a0882b8babf93
ShimmyShong <lattabun@gmail.com>
2023-05-05 12:40:13 -0400

    title and text now get saved into the db.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./routes/index.js'); // TODO ADD THIS LATER
+const fs = require('fs');
+const uniqid = require('uniqid')
 
 const PORT = 3001;
 
@@ -18,6 +19,56 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(`${req.method} request received to get note`)
+    console.log(`${req.method} request received to get note`)
+})
+
+app.post('/api/notes', (req, res) => {
+
+    console.info(`${req.method} request received to add note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uniqid()
+        }
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNote = JSON.parse(data);
+
+                // Add a new review
+                parsedNote.push(newNote);
+
+                // Write updated post back to the file
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNote, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+})
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
